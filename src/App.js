db20caf15de67c09c36ea07fcee9fc5738cabfb3
Paul Godard <paul.godard@outlook.fr>
2020-06-25 12:22:34 +0200

    menu js...

--- src/App.js
+++ src/App.js
@@ -1,53 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars,faTimes } from '@fortawesome/free-solid-svg-icons'
 import './App.scss';
 
-// let toto = [{
-//   nom: "GODARD",
-//   prenom: "Paul"
-// },{
-//   nom: "GILLE",
-//   prenom: "Florian"
-// },{
-//   nom: "MOLLET",
-//   prenom: "Simon"
-// }];
-/* {toto.map((currentObject)=>(
-          <div>
-            {currentObject.nom}
-          </div>
-        ))} */
-
 function App() {
+  const [toggle, setToggle] = useState(false);
+  const toggleMenu = () =>{
+    setToggle(!toggle);
+
+  }
   return (
     <div className="grid-container">
-      <div class="menu-icon">
-        <FontAwesomeIcon icon={faBars} />
+      <div className="menu-icon">
+        <FontAwesomeIcon icon={faBars} onClick={()=>toggleMenu()}/>
       </div>
-      <header class="header">
-        <div class="header-icon">Icon</div>
-        <div class="header-text">Spotify v2</div>
+      <header className="header">
+        <div className="header-icon">Icon</div>
+        <div className="header-text">Spotify v2</div>
       </header>
-      <aside class="sidebar">
-      <div class="sidebar-close">
-      <FontAwesomeIcon icon={faBars} onclick="toggleMenu()"/>
-      </div>
-      <ul class="sidebar-list">
-        <li class="sidebar-item">Item 1</li>
-        <li class="sidebar-item">Item 2</li>
-        <li class="sidebar-item">Item 3</li>
-      </ul>
-    </aside>
+      <aside className={`sidebar ${toggle ? 'active' : ''}`}>
+        <div className="sidebar-close">
+          <FontAwesomeIcon icon={faTimes} onClick={()=>toggleMenu()}/>
+        </div>
+        <ul className="sidebar-list">
+          <li className="sidebar-item">Titres lickés</li>
+          <li className="sidebar-item">Récemment écoutés</li>
+        </ul>
+      </aside>
       <main>
         <div className="search-bar-container">
           <input type="text" id="search-bar" placeholder="Search . . ."></input>
         </div>
         <div className="overview-container">
-          <div class="overview-card">
-            <div class="overview-icon">Overview</div>
-            <div class="overview-text">Card</div>
+          <div className="overview-card">
+            <div className="overview-icon">Overview</div>
+            <div className="overview-text">Card</div>
           </div>
         </div>
       </main>
